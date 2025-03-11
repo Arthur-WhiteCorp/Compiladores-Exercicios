d56d70f21b1a962e675f9c7f133794ca8e3abde4
Arthur-WhiteCorp <arthursilvamatias@gmail.com>
2025-03-10 22:26:59 -0400

    estrutura do repo

---
+++ README.md
@@ -0,0 +1,25 @@
+# DESCRIÇÃO
+
+Repositório de exercícios para a matéria de compiladores 2025/01, contém exercícos em python.
+Qualquer cópia destes exercicios pelos alunos cursantes da disciplina será considerada plágio.
+Alunos devem somente utilizar o material fornecido como base para o desenvolvimento dos exercícios.
+
+## Ferramentas
+
+- [Python](https://www.python.org/)
+- [Antlr](https://www.antlr.org/)
+
+## Dependências
+
+- [antlr4-python3-runtime](https://pypi.org/project/antlr4-python3-runtime/)
+
+instalar com:
+```
+pip install antlr4-python3-runtime
+```
+
+## Autor
+
+- Nome: Arthur Silva Matias
+- E-mail: arthur.matias@icomp.ufam.edu.br
+
---
+++ generate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+ANTLR_PATH="$SCRIPT_DIR/antlr/antlr-4.13.2-complete.jar"
+VISITOR_LISTENER=""
+DEST_LANG="Python3"
+
+if [ -z "$1" ]; then
+    echo "deve passar como argumento o nome da gramatica"
+    echo "./generate.sh <gramatica> <listener|visitor> <python|cpp>"
+    exit 1
+fi
+
+if [ -e "$1" ]; then
+    echo "gramatica $1 nao encontrada"
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+    echo "deve passar como argumento se quer gerar um listener ou visitor"
+    echo "./generate.sh <gramatica> <listener|visitor> <python|cpp>"
+    exit 2
+fi
+
+if [ "$2" != "listener" ] && [ "$2" != "visitor" ]; then
+    echo "segundo argumento não reconhecido"
+    echo "./generate.sh <gramatica> <listener|visitor> <python|cpp>"
+    exit 2
+fi
+
+
+
+if [ "$2" == "listener" ]; then
+    VISITOR_LISTENER="-no-visitor -listener"
+else
+    VISITOR_LISTENER="-no-listener -visitor"
+fi
+
+
+
+java -jar $ANTLR_PATH -Dlanguage=$DEST_LANG $VISITOR_LISTENER ./$1
+
